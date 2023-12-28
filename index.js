84bb33ff632b57f3bc13f80c0c905d7d886901fe
chrvs97 <99564746+chrvs97@users.noreply.github.com>
2023-12-28 14:10:06 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+document.querySelector(".img1").addEventListener("click", function(){
+    alert("USE BACK TO RETURN TO FIRST PAGE!")
+})
+
+
+function toggleDropdown(){
+
+    var dropdown = document.querySelectorAll(".dropdown-content")[0];
+    
+    dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
+
+
+    var dropdown = document.querySelectorAll(".dropdown-content")[1];
+    
+    dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
+
+    var dropdown = document.querySelectorAll(".dropdown-content")[2];
+    
+    dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
+
+
+}
+
+window.onclick = function (event) {
+   if (!event.target.matches('.dropbtn')){
+    var dropdowns = document.querySelectorAll(".dropdown-content")[0]
+
+    dropdowns.style.display = 'none'
+
+    var dropdowns = document.querySelectorAll(".dropdown-content")[1]
+
+    dropdowns.style.display = 'none'
+
+    var dropdowns = document.querySelectorAll(".dropdown-content")[2]
+
+    dropdowns.style.display = 'none'
+
+   }
+}
