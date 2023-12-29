ef6df32e771a2a3d0532b6059507577d4b90115d
chrvs97 <99564746+chrvs97@users.noreply.github.com>
2023-12-29 11:17:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,32 +3,32 @@ function toggleDropdown(){
     var dropdown = document.querySelectorAll(".dropdown-content")[0];
     
     dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
+}
 
-
+function toggleDropdown1(){
     var dropdown = document.querySelectorAll(".dropdown-content")[1];
     
     dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
+}
 
+function toggleDropdown2(){
     var dropdown = document.querySelectorAll(".dropdown-content")[2];
     
     dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
-
-
 }
 
 window.onclick = function (event) {
    if (!event.target.matches('.dropbtn')){
-    var dropdowns = document.querySelectorAll(".dropdown-content")[0]
+    var dropdowns = document.querySelectorAll(".dropdown-content")
 
+    for (i = 0; i < dropdowns.length; i++){
+        openDropdown = dropdowns[i]
+
+        if (openDropdown.style.display === 'block'){
+            openDropdown.style.display = 'none'
+        }
+
+    }
     dropdowns.style.display = 'none'
-
-    var dropdowns = document.querySelectorAll(".dropdown-content")[1]
-
-    dropdowns.style.display = 'none'
-
-    var dropdowns = document.querySelectorAll(".dropdown-content")[2]
-
-    dropdowns.style.display = 'none'
-
    }
 }
